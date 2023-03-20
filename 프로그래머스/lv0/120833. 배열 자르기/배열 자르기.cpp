62f7c8c401e3c12e4611ea45e7d4a16e17f7eeb7
wive10 <70935978+wive10@users.noreply.github.com>
2023-03-20 23:28:47 +0900

    [level 0] Title: 배열 자르기, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> numbers, int num1, int num2) {
+    vector<int> answer;
+    for (int i = 0; i <= num2 - num1; i++) answer.push_back(numbers[num1 + i]);
+    return answer;
+}
