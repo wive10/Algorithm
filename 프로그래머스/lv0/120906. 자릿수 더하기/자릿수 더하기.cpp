95af8288d1ae9c45877cb3b62733bfd44f9594fa
wive10 <70935978+wive10@users.noreply.github.com>
2023-03-20 23:32:38 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.01 ms, Memory: 3.66 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    for (int i = 0; i < n; i / 10) {
+        answer += n % 10;
+        n = n / 10;
+    }
+    return answer;
+}
